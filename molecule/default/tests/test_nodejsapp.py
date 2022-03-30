4a713e7a13ae143b69742215e3fc611a91c3b9ec
Simon Crespeau <simon.crespeau@evilempirestudio.com>
2022-03-30 11:53:47 +0200

    Possibility to clone repository locally before deploying (because of private git intances)

--- molecule/default/tests/test_nodejsapp.py
+++ molecule/default/tests/test_nodejsapp.py
@@ -7,19 +7,19 @@ def test_nodejs_is_installed(host):
     nodejs = host.package("nodejs").version
     assert nodejs == "16.14.2-deb-1nodesource1"
 
-def test_application_main_directory_presence(host):
-    assert host.file("/opt/hello").exists
-    assert host.file("/opt/hello").is_directory
+def test_application_from_git_main_directory_presence(host):
+    assert host.file("/opt/hello-from-git").exists
+    assert host.file("/opt/hello-from-git").is_directory
 
-def test_application_service(host):
-    assert host.service("hello").is_valid
-    assert host.service("hello").is_running
-    assert host.service("hello").is_enabled
+def test_application_from_git_service(host):
+    assert host.service("hello-from-git").is_valid
+    assert host.service("hello-from-git").is_running
+    assert host.service("hello-from-git").is_enabled
 
-def test_application_socket_listening(host):
+def test_application_from_git_socket_listening(host):
     assert host.socket("tcp://3000")
 
-def test_application_reponse_200(host):
+def test_application_from_git_reponse_200(host):
     ip = host.ansible.get_variables()['ansible_host']
     r = requests.get(f"http://{ip}:3000")
     assert r.status_code == 200
